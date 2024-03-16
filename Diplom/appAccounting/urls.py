b87f0e694ed1b3faa47c31755c8f707f5cd4801d
happyhippy78 <148372691+happyhippy78@users.noreply.github.com>
2024-03-16 19:43:11 +0300

    create classes and etc

--- Diplom/appAccounting/urls.py
+++ Diplom/appAccounting/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from .views import UserSignup, UserLogin, PortfolioList
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
-    
+    path('signup/', UserSignup.as_view(), name='signup'),
+    path('login/', UserLogin.as_view(), name='login'), 
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('', PortfolioList.as_view(), name='pfl-list'),
 ]
--- Diplom/appAccounting/views.py
+++ Diplom/appAccounting/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.views.generic import View
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class UserLogin(LoginView):
+    template_name = 'app/signin.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+class UserSignup(FormView):
+    template_name = 'app/signup.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('pfl-list')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(UserSignup, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('pfl-list')
+        return super(UserSignup, self).get(*args, **kwargs)
+    
+class PortfolioList(LoginRequiredMixin,View):
+    def get(self,request):
+        account = User.objects.get(username=request.user)
+        context = {'user':account}
+        return render(request,'app/home.html',context)
+    
+class UserLogin(LoginView):
+    template_name = 'app/signin.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('pfl-list')
