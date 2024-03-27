7543a2c15e8579a7f5838abeb159d3159134db73
happyhippy78 <148372691+happyhippy78@users.noreply.github.com>
2024-03-27 16:38:29 +0300

    add block

--- DiplomProject/appAccounting/admin.py
+++ DiplomProject/appAccounting/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import AccountInfo, ExpenseInfo
 
-# Register your models here.
+@admin.register(AccountInfo)
+class AccountInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'password', 'user_budget']
+    list_display_links = ['username', 'user_budget']
+    list_filter = ['username']
+    list_per_page = 20
+
+@admin.register(ExpenseInfo)
+class ExpenseInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'expense_name', 'cost', 'date_added', 'user_expense']
+    list_display_links = ['expense_name', 'cost', 'date_added', 'user_expense']
+    list_filter = ['expense_name']
+    list_per_page = 20
